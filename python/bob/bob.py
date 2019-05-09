ac64c5b1338b5e3da82a6200ed516bde2c437c6a
Ashok Bakthavathsalam <lifebalance@gmail.com>
2019-05-09 18:53:40 +0000

    fixed failing test_forceful_question

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,14 +1,14 @@
-import string
-
-
 def hey(input):
     out = None
     input = input.strip()
-    
+
     if not input:
         out = "Fine. Be that way!"
     elif input.isupper():
-        out = "Whoa, chill out!"
+        if input[-1] == '?':
+            out = "Calm down, I know what I'm doing!"
+        else:
+            out = "Whoa, chill out!"
     elif input[-1] == '?':
         out = "Sure."
     else:
