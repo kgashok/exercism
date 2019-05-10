0263b75c8b1a29a3e64cf6c15112c12ed3138257
Ashok Bakthavathsalam <lifebalance@gmail.com>
2019-05-10 07:00:28 +0000

    simplified using top variable for stack

--- c/matching-brackets/src/matching_brackets.c
+++ c/matching-brackets/src/matching_brackets.c
@@ -10,25 +10,25 @@ const char* closers = "]})";
 
 bool is_paired(const char* code) {
     char *stack = calloc(100, 1);
+    int top = -1; // empty stack
 
     for (; *code; code++) {
         const char* idx;
         // push the corresponding closer for an opener
         // on to the stack
         if((idx = strchr(openers, *code))){
-            stack[strlen(stack)] = closers[idx-openers];
-            stack[strlen(stack)+1] = '\0';
+            stack[++top] = closers[idx-openers];
         }
         // does the closer match the token on the top of the
         // stack?
         if(strchr(closers, *code)) {
-            if (stack[strlen(stack)-1] == *code)
-                stack[strlen(stack)-1] = '\0';
+            if (stack[top] == *code)
+                top--;
             else
                 return false;
         }
     }
     // if the stack is empty, then pairs are balanced
-    return strlen(stack)? false: true;
+    return top == -1? true: false;
 
 }
