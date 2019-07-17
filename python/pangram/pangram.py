c9a0bef25d3039b9bc3bb8a2c68a93fac284320d
Ashok Bakthavathsalam <lifebalance@gmail.com>
2019-07-17 18:35:37 +0000

    improved solution for pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,5 +1,18 @@
+"""Pangram Checker
+This function checks whether a string of characters is a pangram.
+"""
+
 from string import ascii_lowercase as english_alphabet
 
 
-def is_pangram(string):
-    return all(c in string.lower() for c in english_alphabet)
+def is_pangram(text):
+    '''
+    >>> is_pangram("abcde")
+    False
+    >>> is_pangram("a lazy brown fox quickly jumped over the green fence")
+    True
+    '''
+
+    alphabet = set(english_alphabet)
+    text = set(text.lower())
+    return alphabet.issubset(text)
